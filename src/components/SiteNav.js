606c1cec5277367195f6a8e2fd2cf90a2857fd73
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-11-17 22:53:08 -0500

    se creo un archivo de estilos para el componente SiteNav

--- src/components/SiteNav.js
+++ src/components/SiteNav.js
@@ -1,21 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from "react-router-dom";
+import './styles/SiteNav.css'
 
 
 const SiteNav = props => {
   return (
-    <nav
-      style={{
-        // borderBottom: "solid 1px",
-        // paddingBottom: "1rem"
-        fontWeight: "200",
-      }}
-    >
-      <Link style={{ "text-decoration": "none" , color: "inherit" , 'margin': '0 1rem 0 2rem' }} to="/">Inicio</Link>
-      <Link style={{ "text-decoration": "none" , color: "inherit" , 'margin': '0 1rem' }} to="/productos">Productos</Link>
-      <Link style={{ "text-decoration": "none" , color: "inherit" , 'margin': '0 1rem' }} to="/nosotros">Nosotros</Link>
-      <Link style={{ "text-decoration": "none" , color: "inherit" , 'margin': '0 1rem' }} to="/contacto">Contacto</Link>
+    <nav className="NavBar">
+      <Link className="NavItem" to="/">Inicio</Link>
+      <Link className="NavItem" to="/productos">Productos</Link>
+      <Link className="NavItem" to="/nosotros">Nosotros</Link>
+      <Link className="NavItem" to="/contacto">Contacto</Link>
     </nav>
   )
 }
