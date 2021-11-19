a9f53b0ee69f444a4f20cfb7f9fb53fce4810afa
Fernando Torres <tp.fernando@gmail.com>
2021-11-18 22:14:30 -0300

    Se maquetó HTML, CSS + responsive de componente ProductFilter

--- src/views/Productos.js
+++ src/views/Productos.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import SiteNav from "../components/SiteNav";
+import ProductsFilter from '../components/ProductsFilter';
 
 const Productos = props => {
   return (
     <div>
       <SiteNav />
+      <br/>
+      <ProductsFilter />
       <h1>Productos</h1>
     </div>
   )
