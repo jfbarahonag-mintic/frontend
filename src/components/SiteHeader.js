372e728fb8bd8868302662f32c3890f22007aa04
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-11-18 23:55:18 -0500

    los elementos svg fueron cambiados

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -36,18 +36,17 @@ function SiteHeader() {
                                 onMouseEnter={() => { setHovered(true) }}
                                 onMouseLeave={() => { setHovered(false) }}
                             >
-                                <svg width="17" height="22" viewBox="0 0 17 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <line x1="1.152" y1="21.47" x2="6.152" y2="13.47" stroke="#898989" strokeWidth="2"/>
-                                    <circle cx="9.5" cy="7.5" r="6.5" stroke="#898989" strokeWidth="2"/>
-                                </svg>
+                                <div>
+                                    <svg width="24" height="24" viewBox="0 0 24 24"><path d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" /></svg>
+                                </div>
+                                    
                             </button>
                         </form>
                     </nav>
                     <div className="User">
-                        <svg width="30" height="30" viewBox="0 0 56 69" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="28.5" cy="16.5" r="16.5" fill="#898989"/>
-                            <path d="M0 68.3761C0 27.3763 55.5 28.3761 55.5 68.3761L0 68.3761Z" fill="#898989"/>
-                        </svg>
+                        <div>
+                            <svg width="24" height="24" viewBox="0 0 24 24"><path d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z"/></svg>
+                        </div>
                     </div>
                 </div>
             </div>
