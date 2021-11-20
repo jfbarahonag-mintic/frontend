4a0f994a3eef9bc5f0298986918a83bc5580ae70
nhoragarciab <nhoragarciab@yahoo.com>
2021-11-19 23:21:18 -0500

    Componente SiteFfooter

---
+++ src/components/SiteFooter.js
@@ -0,0 +1,37 @@
+import FacebookLogo from '../assets/facebook-icon.svg'
+import InstagramLogo from '../assets/instagram-icon.svg'
+import YoutubeLogo from '../assets/youtube-icon-red.svg'
+import './SiteFooter.css'
+
+const SiteFooter= () => {
+    return (
+    <div>
+        <div className="site-footer">
+            <a href="" className="site-footer__logo" target="_blank">
+                <img 
+                    src= {FacebookLogo}
+                    alt="Facebook"
+                />
+            </a>
+            <a href="" className="site-footer__logo" target="_blank">
+                <img 
+                    src= {InstagramLogo}
+                    alt="Instragram"
+                />
+            </a>
+            <a href="" className="site-footer__logo" target="_blank">
+                <img 
+                    src= {YoutubeLogo}
+                    alt="Youtube"
+                />
+            </a>
+    </div>
+    <div className="site-footer__footprint">
+            <p className="site-footer__footprint-text">
+                @2021 - Todos los derechos reservados.
+            </p> 
+         </div>
+    </div>
+    )
+}
+export default SiteFooter
