0868bb48e0ea3fdbc5f59d53334c877c8c2b4062
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-11-22 14:08:36 -0500

    se movio el componente product card a producto y se creo de nuevo el componente product card de manera correcta

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,68 +4,20 @@ import useMediaQuery from './MediaQuery'
 import './ProductCard.css'
 
 function ProductCard({ data }) {
-
-    const { title, urls, characteristics, price, description } = data
-    
-    let [mainImage, setMainImage] = useState(urls[0])
-    let [smallImages, setSmallImages] = useState(urls)
-    let isPageWide = useMediaQuery('(min-width: 768px)')
-
-    const handleClick = (e) => {
-        if (e.target.src !== undefined && e.target.src !== mainImage) {
-            setMainImage(e.target.src)
-        }
-    }
-
-    const listImages = smallImages.map((val, idx) => 
-            <li
-                className="product-card__overview--images-list_item"
-                key={idx}
-            >
-                <img src={val} alt={idx} onClick={ handleClick }/>
-            </li>
-    )
-
-    const listCharacteristics = characteristics.map((val, idx) => <li key={idx}> { val } </li>)
-
-    return ( 
+    const { title, url, price } = data
+    return (
         <div className="product-card">
-            <div className={ isPageWide ? "product-card__overview" : "product-card__overview-mobile" }>
-                <div className={ isPageWide ? "product-card__overview--images" : "product-card__overview--images-mobile" }>
-                    <div className="product-card__overview--images-list">
-                        <ul>
-                            {listImages}
-                        </ul>
-                    </div>
-                    <div className={ isPageWide ? "product-card__overview--images-main" : "product-card__overview--images-main-mobile" } >
-                        <img src={ mainImage } alt="main" />
-                    </div>
-                </div>
-                <div className={ isPageWide ? "product-card__overview--info" : "product-card__overview--info-mobile" }>
-                    <div className="product-card__overview--info-title">
-                        { title }
-                    </div>
-                    <p className="product-card__overview--info-paragraph">
-                        { description }
-                    </p>
-                    <div className="product-card__overview--info-price">
-                        ${ price }
-                    </div>
-                    <button className="product-card__overview--info-button">
-                        Button
-                    </button>
-                </div>
-            </div>
-            <div className="product-card__characteristics">
-                <div className="product-card__characteristics--title">CHARACTERISTICS</div>
-                <div className={ isPageWide ? "product-card__characteristics--list" : "product-card__characteristics--list-mobile" }>
-                    <ul>
-                        {listCharacteristics}
-                    </ul>
-                </div>
-            </div>
+            <span className="product-card__title">
+                { title }
+            </span>
+            {/* <button className="product"> */}
+            <img className="product-card__img" src={ url } alt="product" />
+            {/* </button> */}
+            <span className="product-card__price">
+                ${ price }
+            </span>
         </div>
-    );
+    )
 }
 
 export default ProductCard;
