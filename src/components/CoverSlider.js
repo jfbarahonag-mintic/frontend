1f3709f0b6a918c46c9e0db226e4a6f703b43a62
Geiner <davidlararada1062@hotmail.com>
2021-11-24 20:52:14 -0500

    coverSlider terminado

--- src/components/CoverSlider.js
+++ src/components/CoverSlider.js
@@ -1,21 +1,24 @@
 import React from "react";
 import Slider from "infinite-react-carousel";
+// import prevArrowP from "../asset/left1-arrow.png";
+// import nextArrowP from "../asset/next.png";
 
 import "./CoverSlider.css";
-
 const CoverSlider = ({ images }) => {
+  const settings = {
+    autoplay: true,
+  };
+
   return (
     <section className="slider">
       <h1 className="slider__title">Carousel con infinite-react-carousel</h1>
-      <Slider className="slider__content">
+      <Slider className="slider__content" {...settings}>
         {images.map((image) => (
-          <div
-            key={image.id}
-            className="slider__content--item"
-            style={{ backgroundImage: `url(${image.image})` }}
-          >
-            {/* <img src={image.image} alt={image.title} /> */}
-            <p className="slider-description">{image.title}</p>
+          <div key={image.id} className="slider__content--item">
+            <a href="#">
+              <img src={image.image} alt={image.title} />
+              <p className="slider-description">{image.title}</p>
+            </a>
           </div>
         ))}
       </Slider>
