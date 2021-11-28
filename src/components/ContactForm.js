fdee530fe6d75a381931b087283698b3591e125e
Fernando Torres <tp.fernando@gmail.com>
2021-11-28 13:19:57 -0300

    Se maquetó la vista Contacto

---
+++ src/components/ContactForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactForm = () => {
+  return (
+    <div>
+      ContactForm Component
+    </div>
+  )
+}
+
+export default ContactForm
--- src/views/Contacto.js
+++ src/views/Contacto.js
@@ -1,19 +1,27 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import SiteHeader from "../components/SiteHeader";
 import PageTitle from '../components/PageTitle';
+import ContactForm from '../components/ContactForm';
+import SiteFooter from '../components/SiteFooter';
+
+import './Contacto.css'
 
 const Contacto = props => {
   return (
     <div>
       <SiteHeader />
-      <PageTitle title="CONTACTO"/> 
+      <PageTitle title="CONTACTO"/>
+      <main className="contact-page box">
+        <div className="box__container">
+          <h2 className="contact-page__title">¡ESCRIBANOS UN MENSAJE!</h2>
+          <div className="contact-page__form">
+            <ContactForm />
+          </div>
+        </div>
+      </main>
+      <SiteFooter />
     </div>
   )
-}
-
-Contacto.propTypes = {
-
 }
 
 export default Contacto
