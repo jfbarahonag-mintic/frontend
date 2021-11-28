3ef6acaf4fca672976dfa4c212c644288dfe0b65
Geiner <davidlararada1062@hotmail.com>
2021-11-27 22:00:21 -0500

    Se maketo la vista del Login

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -1,34 +1,55 @@
-import React, { useContext } from 'react'
-import { useNavigate } from 'react-router'
-import { AuthContext } from "../../auth/authContext"
-import { types } from "../../types/types"
-
+import React, { useContext } from "react";
+import { useNavigate } from "react-router";
+import { AuthContext } from "../../auth/authContext";
+import { types } from "../../types/types";
+import "./Login.css";
+import imgLogin from "../../assets/Login.jpg";
 const Login = () => {
-
-  const navigate = useNavigate()
-  const { user, dispatch } = useContext( AuthContext )
+  const navigate = useNavigate();
+  const { user, dispatch } = useContext(AuthContext);
 
   const handleLogin = () => {
-    
     const action = {
       type: types.login,
-      payload: { name: 'Fernando Torres' }
-    }
+      payload: { name: "Fernando Torres" },
+    };
 
-    dispatch(action)
+    dispatch(action);
 
-    const lastPath = localStorage.getItem('lastPath')
+    const lastPath = localStorage.getItem("lastPath");
 
-    navigate('/admin')
-
-  }
+    navigate("/admin");
+  };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={ handleLogin } >Login</button>
+    <div className="container">
+      <div className="container-content">
+        <div className="container-form">
+          <h1 className="container-form__title">Acceder</h1>
+          <form action="">
+            <div>
+              <label>
+                Correo
+                <input type="text" />
+              </label>
+            </div>
+            <div>
+              <label>
+                Contraseña
+                <input type="text" />
+              </label>
+            </div>
+            <div className="conatiner-button">
+              <button className="container-button__form" onClick={handleLogin}>Acceder</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="container-img">
+        <img src={imgLogin} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
