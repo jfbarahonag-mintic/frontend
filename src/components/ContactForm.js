0280c2a4c181613e19f1adbe97f65998468c86b8
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-11-28 22:16:10 -0500

    se agrego la funcionalidad de enviar correos a traves de EmailJS

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,33 +1,55 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import emailjs, { init } from 'emailjs-com'
 
 import './ContactForm.css'
+
+const EMAILJS = {
+  //TODO: Change credentials to PhoneBox account and hide them
+  user_id: "user_HD278ebmTPb2FQEswUWJW",
+  service_id: "service_jno9n4u",
+  template_id: "contact_form",
+};
 
 const ContactForm = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+
+  const form = useRef()
   
   const onSendMessage = e => {
     e.preventDefault();
-    
     if (email !== '' && name !== '' && message !== '') {
-      console.log(`Name: ${name}`);
-      console.log(`Email: ${email}`);
-      console.log(`Message: ${message}`);
+      init(EMAILJS.user_id);
+      emailjs
+        .sendForm(
+          EMAILJS.service_id,
+          EMAILJS.template_id,
+          form.current,
+          EMAILJS.user_id
+        ).then(
+          (response) => {
+            console.log(response);
+          },
+          (err) => {
+            console.log(err.text);
+          }
+        );
     } else {
       /* ERROR: Should never reach this point */
     }
-
   }
 
   return (
     <div className="contact">
       <form
+        ref={form}
         className="contact-form"
         onSubmit={onSendMessage}
       >
         <label htmlFor="name">Nombre:</label>
         <input
+          name="from_name"
           type="text"
           id="name"
           required
@@ -36,6 +58,7 @@ const ContactForm = () => {
         />
         <label htmlFor="email">Correo:</label>
         <input
+          name="from_email"
           type="email"
           id="email"
           required
@@ -44,6 +67,7 @@ const ContactForm = () => {
         />
         <label htmlFor="message">Mensaje:</label>
         <textarea
+          name="message"
           id="message"
           required
           value={message}
