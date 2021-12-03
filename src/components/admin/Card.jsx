88bf5045a200fdefce6db0bbc2f85be9605a74e1
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-12-03 00:45:33 -0500

    se agrego el manejador de evento para que aparezca un modal cuando se le da click en agregar usuario

--- src/components/admin/Card.jsx
+++ src/components/admin/Card.jsx
@@ -1,19 +1,55 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import { Box } from "@mui/system";
+import { Modal } from "@mui/material";
+import { Form } from "@mui/material";
+import { Typography } from "@mui/material";
 
 import { TableUsers } from "./TableUsers";
 
 //TODO: Rename this Component
 function Card({ type, titles, rows }) {
+  const formModalRef = React.useRef();
+
+  const handleClick = () => {
+    formModalRef.current.handleOpen(true);
+  };
+
   return (
     <div style={{ padding: "1rem" }}>
+      <FormModal ref={formModalRef} />
       <Box display="flex" justifyContent="flex-end" mb="1rem">
-        <Button variant="contained">{`Crear ${type}`}</Button>
+        <Button variant="contained" onClick={handleClick}>
+          {`Crear ${type}`}
+        </Button>
       </Box>
-      <TableUsers titles={[...titles, '']} rows={rows} />
+      <TableUsers titles={[...titles, ""]} rows={rows} />
     </div>
   );
 }
+
+const FormModal = React.forwardRef((props, ref) => {
+  
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => setOpen(false);
+
+  // React.useEffect(() => {
+  //   console.log(2);
+  // }, [open])
+
+  React.useImperativeHandle(ref, () => ({
+    handleOpen: (open) => {
+      setOpen(open);
+    },
+  }));
+
+  return (
+    <Modal open={open} onClose={handleClose}>
+      <Typography id="modal-modal-title" variant="h6" component="h2">
+        Text in a modal
+      </Typography>
+    </Modal>
+  );
+});
 
 export { Card };
