61f626c08875d8d1281f9bedf9f5aa8dd8365826
Fernando Torres <tp.fernando@gmail.com>
2021-12-03 22:48:11 -0300

    Se creo componente ProdutForm y se importo en las vistas Edit y Create de Product

--- src/api.js
+++ src/api.js
@@ -1,7 +1,23 @@
 const url = 'http://localhost:4000/api/'
 
+const options = {
+  mode: 'cors',
+  cache: 'no-cache',
+  credentials: 'same-origin',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  redirect: 'follow', 
+  referrerPolicy: 'no-referrer',
+}
+
+// products
 const getProducts = () => {
   return fetch(`${url}products`)
+}
+
+const getProductById = (id) => {
+  return fetch(`${url}products/show/${id}`)
 }
 
 const getProductBySlug = (slug) => {
@@ -12,6 +28,22 @@ const getProductsByCategory = (id) => {
   return fetch(`${url}products/category/${id}`)
 }
 
+const storeProduct = (data) => {
+  return fetch(`${url}products/store`, {
+    method: 'POST',
+    ...options,
+    body: JSON.stringify(data)
+  })
+}
+
+const updateProduct = (id, data) => {
+  return fetch(`${url}products/update/${id}`, {
+    method: 'PUT',
+    ...options,
+    body: JSON.stringify(data)
+  })
+}
+
 // Categories
 const getCategories = () => {
   return fetch(`${url}categories/`)
@@ -20,8 +52,11 @@ const getCategories = () => {
 module.exports = {
   // products
   getProducts,
+  getProductById,
   getProductBySlug,
   getProductsByCategory,
+  storeProduct,
+  updateProduct,
   // categories
   getCategories
 }
--- src/views/admin/products/ProductEdit.jsx
+++ src/views/admin/products/ProductEdit.jsx
@@ -1,10 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Breadcrumbs, Container, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import { useNavigate, useParams } from 'react-router'
+import ProductForm from '../../../components/admin/ProductForm'
+import { getProductById, updateProduct } from '../../../api'
+import ProductDeleteForm from '../../../components/admin/ProductDeleteForm'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import { Link } from 'react-router-dom'
 
 const Edit = () => {
+
+  const navigate = useNavigate()
+
+  const { id } = useParams()
+
+  const [product, setProduct] = useState({})
+
+  // const BreadcrumbsLinks = [
+  //   <Link key="1" to="/admin">Dashboard</Link>,
+  //   <Link key="2" to="/admin/products">Productos</Link>,
+  //   <Link key="3" to="#">Editar</Link>
+  // ]
+
+  useEffect(() => {
+    getProductById(id)
+    .then(resp => resp.json())
+    .then(data => setProduct(data))
+    .catch(err => console.log(err.message))
+  }, [id])
+
+  const handleSubmit = (data) => {
+    updateProduct(id, data)
+    .then(resp => {
+      if (resp.ok) {
+        alert('El producto se actualizó correctamente')
+        navigate('/admin/products')
+      }
+    })
+    .catch(err => console.log(err.message))
+  }
+  
+  const handleDiscard = () => {
+    navigate('/admin/products')
+  }
+
   return (
-    <div>
-      <h1>Edit Product</h1>
-    </div>
+    <>
+      {/* <Breadcrumbs
+        separator={<NavigateNextIcon fontSize="small" />}
+        aria-label="breadcrumb"
+      >
+        { BreadcrumbsLinks }
+      </Breadcrumbs> */}
+
+      <Container maxWidth="md">
+        <Typography variant="h4" mt={3}>
+          Editar producto
+        </Typography>
+
+        <Box my={3}>
+          <ProductForm
+            actionButton="Actualizar"
+            { ...product }
+            handleSubmit={ handleSubmit }
+            handleDiscard={ handleDiscard }
+          />
+        </Box>
+
+        <Box my={5}>
+          <ProductDeleteForm />
+        </Box>
+      </Container>
+    </>
   )
 }
 
