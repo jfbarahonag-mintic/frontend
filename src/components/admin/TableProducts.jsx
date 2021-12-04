ec040c5b2b6a234bac453649d73b2ca0bd2619b6
Geiner <davidlararada1062@hotmail.com>
2021-12-04 09:52:23 -0500

    se agrego button create products

--- src/components/admin/TableProducts.jsx
+++ src/components/admin/TableProducts.jsx
@@ -9,8 +9,9 @@ import Paper from "@mui/material/Paper";
 import EditIcon from "@mui/icons-material/Edit";
 import CircleIcon from "@mui/icons-material/Circle";
 import VisibilityIcon from "@mui/icons-material/Visibility";
-import { IconButton } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
+import { Box } from "@mui/system";
 
 const ProductsTableRow = ({ row }) => {
   return (
@@ -38,47 +39,45 @@ const ProductsTableRow = ({ row }) => {
     </TableRow>
   );
 };
-
 function TableProducts({ titles, rows }) {
   const [content, setContent] = React.useState(rows || []);
 
-  // titles.push("");
-
-  // let row = rows.map((row, idx) => {
-  //   row.edit = <EditIcon id={idx} style={{ cursor: "pointer" }} />;
-
-  // });
-
   React.useEffect(() => {
-    // if (typeof rows[0]._id !== "undefined") {
-
     setContent(rows);
-    // }
   }, [rows]);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {titles.map((title, idx) => (
-              <TableCell
-                align="center"
-                key={idx}
-                style={{ fontWeight: "bold" }}
-              >
-                {title}
-              </TableCell>
+    <>
+      <Box display="flex" justifyContent="flex-end">
+        <Link to={`/admin/products/create`}>
+          <Button variant="contained" style={{ marginBottom: "10px" }}>
+            Crear Producto
+          </Button>
+        </Link>
+      </Box>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {titles.map((title, idx) => (
+                <TableCell
+                  align="center"
+                  key={idx}
+                  style={{ fontWeight: "bold" }}
+                >
+                  {title}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {content.map((row) => (
+              <ProductsTableRow row={row} key={row._id} />
             ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {content.map((row) => (
-            <ProductsTableRow row={row} key={row._id} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
 
