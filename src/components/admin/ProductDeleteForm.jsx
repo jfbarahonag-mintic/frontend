00ce5a4c2d3f3e6b347e9f744ceaebf8b1856fc6
nhoragarciab <nhoragarciab@yahoo.com>
2021-12-05 17:30:17 -0500

    avance componente delete product

--- src/components/admin/ProductDeleteForm.jsx
+++ src/components/admin/ProductDeleteForm.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import { Button, Divider, FormControl, InputAdornment, InputLabel, MenuItem, OutlinedInput, Select, Stack, TextField, Typography } from '@mui/material';
 
-const ProductDeleteForm = () => {
+const ProductDeleteForm = ({ id }) => {
+
+  const handleSubmit = () =>{
+    console.log("handleSubmit")
+  }
+
   return (
-    <div>
-      ProductDeleteForm
-    </div>
+    <Box
+    component="form"
+    noValidate
+    autoComplete="off"
+    onSubmit={ handleSubmit }
+    sx={{ 
+      padding: '24px', 
+      boxSizing: 'border-box',
+      boxShadow: '0 0 6px #ccc',
+      borderRadius: '8px', 
+    }}
+  >
+    <Typography variant="h6">
+      ¿Desea eliminar este producto?
+    </Typography>
+    
+    <Button 
+          type="submit"
+          variant="contained"
+        >
+          Eliminar
+    </Button>
+    
+  </Box>
   )
 }
 
